504fff0e910d6a900e61b163125e90548f7e5df9
Tanel Mae <tanel.mae@gmail.com>
2019-11-12 09:58:26 +0200

    Cleanup denv formula

--- Formula/denv.rb
+++ Formula/denv.rb
@@ -1,25 +1,14 @@
 class Denv < Formula
   desc "Docker Compose wrapper for git based projects"
   homepage "https://gist.github.com/tanelmae/e596c8ea00abaa40b51db75795394b88"
-  url "https://raw.githubusercontent.com/tanelmae/denv/master/bin/denv",
-    :using => :nounzip
-  sha256 "301097f43ddecfb9ec312e9601e1c313ce3733d72021616ed86b650dfc012719"
+  url "https://github.com/tanelmae/denv/archive/v0.1.2.zip"
+  sha256 "7232724ca665c09e0e52309d306812627da45cf05efa29197397e22de80b2e42"
   depends_on "git"
-  version "0.0.1"
-
-  resource "bash-completion" do
-    url "https://raw.githubusercontent.com/tanelmae/denv/master/denv.bash-completion"
-    sha256 "a27661680a653136c15d1723c5b029a529eee7e6ac874df62c3b5ba752510185"
-  end
+  version "0.1.2"
 
   def install
-    cp 'denv-1', 'denv'
-    chmod 0755, 'denv'
     bin.install "denv" => "denv"
-    resource("bash-completion").stage {
-      cp 'denv.bash-completion', '/usr/local/etc/bash_completion.d/denv.bash-completion'
-      ohai 'Bash completion installed to /usr/local/etc/bash_completion.d/denv.bash-completion'
-    }
+    cp 'denv.bash-completion', '/usr/local/etc/bash_completion.d/denv.bash-completion'
   end
 
   test do
