1a1e6cbc4af870447a031736aa96c3eca3cca9fc
Tanel Mae <tanel.mae@gmail.com>
2020-01-07 20:35:57 +0200

    Update nsc to 0.3.16

--- Formula/nsc.rb
+++ Formula/nsc.rb
@@ -1,15 +1,23 @@
 class Nsc < Formula
   desc "A tool for creating NATS account and user access configurations"
   homepage "https://github.com/nats-io/nsc"
-  url "https://github.com/nats-io/nsc/releases/download/0.3.14/nsc-darwin-amd64.zip"
-  sha256 "40a8f92b7fad042a807fdc9b692d044ee739939976ede5bfb69c830980722efd"
-  version "0.3.14"
+  version "0.3.16"
+
+  if OS.mac?
+    url "https://github.com/nats-io/nsc/releases/download/0.3.16/nsc-darwin-amd64.zip"
+    sha256 "788930a2096d847b0bb950d848fcc5614781a602e4edd51dabbe92f7e6d13418"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/nats-io/nsc/releases/download/0.3.16/nsc-linux-amd64.zip"
+      sha256 "8e3dfe28eac4793c6f1c1400bb3b31785a30e589078ff577b1420baa3ae13f97"
+    end
+  end
 
   def install
     bin.install "nsc" => "nsc"
   end
 
   test do
-    system "false"
+    system "#{bin}/nsc --version"
   end
 end
