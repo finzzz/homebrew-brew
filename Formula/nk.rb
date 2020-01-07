5404a8dcc1077802bbc4729a5a2d3e0def5e5ba3
Tanel Mae <tanel.mae@gmail.com>
2020-01-07 20:29:14 +0200

    Update nk version to 0.1.3

--- Formula/nk.rb
+++ Formula/nk.rb
@@ -1,14 +1,14 @@
 class Nk < Formula
   desc "The nk utility program can be used to generate nkeys, signing, and verify signatures"
   homepage "https://nats-io.github.io/docs/nats_tools/nk.html"
-  url "https://github.com/nats-io/nkeys/archive/v0.1.2.zip"
-  sha256 "e5a9f47d342395d1d16dfede154b2d42854443bd3843fa91c6d8a70fb5d8c876"
-  version "0.1.2"
+  url "https://github.com/nats-io/nkeys/archive/v0.1.3.zip"
+  sha256 "9afbf8c506d40312c801b97986b8f496ddb954fc04cfc746e85b9aa4de79d616"
+  version "0.1.3"
   depends_on "go"
 
   def install
     system "go", "mod", "vendor"
-    system "go", "build", "-mod", "vendor", "-o", "nk/nk", "nk/main.go"
+    system "go", "build", "-mod", "vendor", "-ldflags", "-X main.Version=v0.1.3", "-o", "nk/nk", "nk/main.go"
     bin.install "nk/nk" => "nk"
   end
 
